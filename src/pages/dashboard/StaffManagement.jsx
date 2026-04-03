953ea2fcbe2a9d6dfd6e92a0d6dd05b16951cf91
Shrihari Suresh Kasar <shreeharikasar@gmail.com>
2026-04-03 11:30:35 +0530

    upd

--- src/pages/dashboard/StaffManagement.jsx
+++ src/pages/dashboard/StaffManagement.jsx
@@ -97,7 +97,7 @@ const fetchStaff = async () => {
       {/* ADD STAFF */}
       <button
         className="submit-btn"
-        onClick={() => navigate("admin/staff")}
+        onClick={() => navigate("/admin/staff")}
       >
         + Add Staff
       </button>
